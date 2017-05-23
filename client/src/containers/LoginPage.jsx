22d9ef53cae8480bd5caf97b1abffbcb48332fba
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2017-05-22 18:37:02 -0700

    updating packages (DO NOT UPDATE REACT ROUTER) / fixed PropType error

--- client/src/containers/LoginPage.jsx
+++ client/src/containers/LoginPage.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Auth from '../modules/Auth';
 import LoginForm from '../components/LoginForm.jsx';
 
